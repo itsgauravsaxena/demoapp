266fe3f60f87b0fe5299f453940b8b6b2edfa909
Gaurav Saxena <gauravsaxena.ec@gmail.com>
2019-06-11 14:57:39 +0200

    ui parts

--- src/main/java/com/gs/demo/controller/UserController.java
+++ src/main/java/com/gs/demo/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
-
+@
   private final UserRepository userRepository;
 
   public UserController(UserRepository userRepository) {
